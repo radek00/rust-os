801fdaba02e9efbd1f3fb633cca0ec7dcbca9c78
Radekor500 <radek00500@gmail.com>
2024-09-21 15:12:46 +0300

    print macro implementation

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,8 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+pub extern "C" fn _start() {
+    println!("Hello World{}", "!");
 
     loop {}
 }
