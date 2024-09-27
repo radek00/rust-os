6afd361e1d7f62139d88aece7c613932410a3caa
Radekor500 <radek00500@gmail.com>
2024-09-27 17:48:38 +0300

    extract common function to lib and setup integration tests

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -2,31 +2,24 @@
 #![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(rust_os::test_runner)]
+
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use rust_os::println;
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rust_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
