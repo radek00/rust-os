17c109f8857ed82fc63e3c052c56f340e028a5fd
Radekor500 <radek00500@gmail.com>
2024-10-29 20:56:42 +0100

    example level 4 table reads

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,39 @@
 
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use rust_os::{hlt_loop, println};
+use rust_os::{hlt_loop, memory::active_level_4_table, println};
+use x86_64::VirtAddr;
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("Hello World{}", "!");
 
-    rust_os::init(); // new
+    rust_os::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
+
+    for (i, entry) in l4_table.iter().enumerate() {
+        if !entry.is_unused() {
+            println!("L4 Entry {}: {:?}", i, entry);
+        }
+    }
 
     // unsafe {
     //     *(0xdeadbeef as *mut u8) = 42;
     // };
 
-    let ptr = 0x2031b2 as *mut u8;
+    // let ptr = 0x2031b2 as *mut u8;
 
-    // read from a code page
-    unsafe {
-        let x = *ptr;
-    }
-    println!("read worked");
+    // // read from a code page
+    // unsafe {
+    //     let x = *ptr;
+    // }
+    // println!("read worked");
 
-    // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3();
+    // // invoke a breakpoint exception
+    // x86_64::instructions::interrupts::int3();
 
     #[cfg(test)]
     test_main();
