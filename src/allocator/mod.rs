d193c85f0b01639da242194e2df11d1c0ab9623a
Radekor500 <radek00500@gmail.com>
2024-11-03 12:59:21 +0100

    bump allocator implementation

--- src/allocator/mod.rs
+++ src/allocator/mod.rs
@@ -1,4 +1,4 @@
-use linked_list_allocator::LockedHeap;
+use bump::{BumpAllocator, Locked};
 use x86_64::{
     structures::paging::{
         mapper::MapToError, FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB,
@@ -6,8 +6,10 @@ use x86_64::{
     VirtAddr,
 };
 
+pub mod bump;
+
 #[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
+static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
